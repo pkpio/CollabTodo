50a219c602a15deca5b30afedfb541089c9f8cc0
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-05-08 15:49:40 +0200

    Init params

---
+++ app/src/main/java/params/Global.java
@@ -0,0 +1,14 @@
+package params;
+
+/**
+ * Created by praveen on 8/5/15.
+ */
+public class Global {
+    public static final int METHOD_CREATE_LIST = 1;
+    public static final int METHOD_CREATE_ENTRY = 2;
+    public static final int METHOD_DELETE_LIST = 3;
+    public static final int METHOD_DELETE_ENTRY = 4;
+    public static final int METHOD_MOVE_ENTRY = 5;
+    public static final int METHOD_UPDATE_PRIORITY = 6;
+
+}
