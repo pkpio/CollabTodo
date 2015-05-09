72ec468eec6440a330bf212da8f2cee2d5645bb2
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-05-09 15:50:58 +0200

    Init EntryPriorityDao and most work on Get methods of EntryDao

--- app/src/main/java/closure/space/collabtodo/database/EntryDao.java
+++ app/src/main/java/closure/space/collabtodo/database/EntryDao.java
@@ -1,5 +1,7 @@
 package closure.space.collabtodo.database;
 
+import java.util.List;
+
 import closure.space.collabtodo.models.Entry;
 import closure.space.collabtodo.models.TodoList;
 
@@ -20,9 +22,7 @@ public class EntryDao {
      */
     public void save(Entry entry) {
         entry.save();
-
-        // Handle saving of each priority of this event
-        
+        // -TODO- Handle saving of each EntryPriority of this event
     }
 
 
@@ -41,6 +41,52 @@ public class EntryDao {
      * @param entryid
      */
     public void delete(String entryid) {
-
+        // -TODO- Implement this
     }
+
+    /**
+     * Fetches the Entry from the local database
+     *
+     * @param entryid Entryid of the Entry in local database
+     * @return Entry with given id
+     */
+    public Entry getEntry(String entryid) {
+        List<Entry> entries = Entry.find(Entry.class, "entryid = ?", entryid);
+        if (entries == null || entries.size() == 0)
+            return null;
+        else
+            return setEntryPriorities(entries.get(0));
+    }
+
+    /**
+     * Fetches all Entries in a list from local database
+     *
+     * @param listid TodoListId of the list
+     * @return List of Entries
+     */
+    public List<Entry> getEntries(String listid) {
+        return setEntryPriorities(Entry.find(Entry.class, "listid = ?", listid));
+    }
+
+    /**
+     * Sets all entry priorities corresponding to this event from the local Database.
+     * Since Sugar doesn't support saving or retrieving of Lists, we keep a pointer,
+     * entryid, in each EntryPriority and save, retrieve them accordingly.
+     */
+    private Entry setEntryPriorities(Entry entry) {
+        // -TODO- Retrieve them from EntryPriorityDao and set
+        return entry;
+    }
+
+    /**
+     * Sets all entry priorities corresponding to each event in this List from the local Database.
+     * Since Sugar doesn't support saving or retrieving of Lists, we keep a pointer,
+     * entryid, in each EntryPriority and save, retrieve them accordingly.
+     */
+    private List<Entry> setEntryPriorities(List<Entry> entries) {
+        for (Entry entry : entries)
+            entry = setEntryPriorities(entry);
+        return entries;
+    }
+
 }
---
+++ app/src/main/java/closure/space/collabtodo/database/EntryPriorityDao.java
@@ -0,0 +1,14 @@
+package closure.space.collabtodo.database;
+
+/**
+ * EntryPriority Data Access Object
+ * <p/>
+ * This abstracts the details of reading or updating EntryPriority in / out of the Sugar Database
+ * <p/>
+ * Created by praveen on 9/5/15.
+ */
+public class EntryPriorityDao {
+
+    // -TODO- Add and implement all methods like in the EntryDao class
+
+}
